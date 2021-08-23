86adc2f2b46e3fa2bce7a1e8b6faaae63e68e359
domsaut <domenico.sauta@gmail.com>
2021-08-23 17:34:02 +1200

    added ema

--- momentum_algorithm.py
+++ momentum_algorithm.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from talib.abstract import *
+from sklearn.model_selection import train_test_split
 
 # local imports
 from gemini_modules import engine
@@ -8,31 +9,43 @@ from gemini_modules import engine
 df = pd.read_csv("data/USDT_XRP.csv", parse_dates=[0])
 
 # globals
-training_period = 15
+TRAINING_PERIOD = 15
+ALPHA = 0.3
 
 #backtesting
+# train, test = train_test_split(df, test_size = 0.3, shuffle = False)
 backtest = engine.backtest(df)
 
 '''Algorithm function, lookback is a data frame parsed to function continuously until end of initial dataframe is reached.'''
 def logic(account, lookback):
     try:
         today = len(lookback)-1
-        if(today > training_period): 
-            price_moving_average = lookback['close'].rolling(window=training_period).mean()[today]  # update PMA
-            volumn_moving_average = lookback['volume'].rolling(window=training_period).mean()[today]  # update VMA
+        if(today > TRAINING_PERIOD):
+            # indicator
+            simple_moving_average = lookback['close'].rolling(window=TRAINING_PERIOD).mean()[today]  # update PMA
+            exponential_moving_average = lookback['close'].ewm(alpha = ALPHA, adjust = False).mean()[today] # update EMA
+            INDICATOR = exponential_moving_average
 
-            if(lookback['close'][today] < price_moving_average):
+            # volume filter
+            volumn_moving_average = lookback['volume'].rolling(window=TRAINING_PERIOD).mean()[today]  # update VMA
+
+
+
+            if(lookback['close'][today] < INDICATOR):
                 if(lookback['volume'][today] > volumn_moving_average):
                     if(account.buying_power > 0):
                         account.enter_position('long', account.buying_power, lookback['close'][today])
             else:
-                if(lookback['close'][today] > price_moving_average):
+                if(lookback['close'][today] > INDICATOR):
                     if(lookback['volume'][today] < volumn_moving_average):
                         for position in account.positions:
-                                account.close_position(position, 1, lookback['close'][today]) 
+                                account.close_position(position, 1, lookback['close'][today])
     except Exception as e:
         print(e)
     pass  # Handles lookback errors in beginning of dataset
+
+def apply_fee(account):
+    return 0.999
 
 
 if __name__ == "__main__":
---
+++ testing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+
+# importing data and splitting into training and testing period
+df = pd.read_csv('./data/USDT_XRP.csv')
+train, test = train_test_split(df, test_size = 0.3, shuffle = False)
+
+train['sma_20'] = train['close'].rolling(10).mean()
+train['ema1'] = train['close'].ewm(alpha = 0.1, adjust = False).mean()
+train['ema2'] = train['close'].ewm(alpha = 0.3, adjust = False).mean()
+train[['close','sma_20', 'ema1', 'ema2']].plot(linewidth = 1)
+
+plt.show()
