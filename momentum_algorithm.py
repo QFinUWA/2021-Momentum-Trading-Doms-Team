aac5efda85e054005da6cced34f28cdd2c3b4503
tomothyc <68897511+tomothyc@users.noreply.github.com>
2021-09-02 09:59:29 +0800

    implemented two moving averages

--- momentum_algorithm.py
+++ momentum_algorithm.py
@@ -8,7 +8,7 @@ from gemini_modules import engine
 df = pd.read_csv("data/USDT_XRP.csv", parse_dates=[0])
 
 # globals
-training_period = 15
+training_period = 200
 
 #backtesting
 backtest = engine.backtest(df)
@@ -18,16 +18,17 @@ def logic(account, lookback):
     try:
         today = len(lookback)-1
         if(today > training_period): 
-            price_moving_average = lookback['close'].rolling(window=training_period).mean()[today]  # update PMA
+            long_moving_average = lookback["close"][-200:].mean()  # update PMA
+            short_moving_average = lookback["close"][-50:].mean()
             volumn_moving_average = lookback['volume'].rolling(window=training_period).mean()[today]  # update VMA
 
-            if(lookback['close'][today] < price_moving_average):
-                if(lookback['volume'][today] > volumn_moving_average):
+            if(long_moving_average < short_moving_average):
+                #if(lookback['volume'][today] > volumn_moving_average):
                     if(account.buying_power > 0):
                         account.enter_position('long', account.buying_power, lookback['close'][today])
             else:
-                if(lookback['close'][today] > price_moving_average):
-                    if(lookback['volume'][today] < volumn_moving_average):
+                if(long_moving_average > short_moving_average):
+                    #if(lookback['volume'][today] < volumn_moving_average):
                         for position in account.positions:
                                 account.close_position(position, 1, lookback['close'][today]) 
     except Exception as e:
